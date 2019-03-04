91a1e8709d3d57289ec68b5b26e2cbcd83adb8c8
Rubinder <mail2rubinder@gmail.com>
2019-03-04 13:32:39 +1100

    updated Sample Macros

--- src/SampleConfig/Macros.ts
+++ src/SampleConfig/Macros.ts
@@ -2,7 +2,7 @@ import { Macro } from 'nodehotkey';
 import { KEYCODES as KC } from 'nodehotkey';
 
 export let Macros: Macro = {
-	// Macro 1
+	// Macro 1 - press CTRL+E to trigger
 	'Name of the Macro': {
 		keys: [KC._CONTROL, KC._E],
 		steps: [
@@ -13,26 +13,24 @@ export let Macros: Macro = {
 			{ releaseKey: KC._COMMA }
 		]
 	},
-	// Macro 2
+	// Macro 2 - type 'as/' in notepad or any other text editor to trigger
 	'Use HotStrings': {
 		Hotstring: 'as/',
-		// when there is only one step type the hotstring gets replaced by it.
 		steps: [{ type: "'as/' gets replaced by this line." }]
 	},
-	// Macro 3
+	// Macro 3 - press CTRL+SHIFT+A or type 'ab' to trigger
 	'Macro3': {
 		keys: [KC._CONTROL, KC._SHIFT, KC._A],
 		Hotstring: 'ab',
-		// if there is more than one step the hotstring 'ab' will not be deleted
 		steps: [
 			{ type: `This Macro is triggred by either typing 'ab' or holding CTRL+SHIFT+A. The next step types '$' symbol on screen 2 times ` },
 			{ click: { key: KC._4, modifier: KC._SHIFT, times: 2 } }
 		]
 	},
-	// Macro 4:
+	// Macro 4: - type 'long' in notepad or any other text editor to trigger
+	// we use 'paste' instead of 'type' for longer text
 	'Long Text': {
-		Hotstring: 'long', // type 'long' in notepad
-		// for long text use paste instead of type
+		Hotstring: 'long',
 		steps: [{ paste: `"The quick brown fox jumps over the lazy dog" is an English-language pangram—a sentence that contains all of the letters of the alphabet. It is commonly used for touch-typing practice, testing typewriters and computer keyboards, displaying examples of fonts, and other applications involving text where the use of all letters in the alphabet is desired. Owing to its brevity and coherence, it has become widely known.` }]
 	}
 }
