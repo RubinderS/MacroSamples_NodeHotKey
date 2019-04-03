5ec37f676b87a7f10f7e0850e931b4e5b94a2e0f
Rubinder <mail2rubinder@gmail.com>
2019-04-03 20:52:19 +1100

    updated index file

--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,4 @@ let nhk = new NodeHotKey(Macros);
 
 nhk.startListening();
 
-nhk.on(nhk.eventTypes.keyReleased, (eventData: any) => {
-    console.log('KeyReleased', eventData.keyCode);
-});
-
-nhk.on(nhk.eventTypes.keyPressed, (eventData: any) => {
-    console.log('KeyPressed', eventData.keyCode);
-});
-
-nhk.on(nhk.eventTypes.mouseKeyReleased, (eventData: any) => {
-    console.log('mouseKeyReleased', eventData.keyCode);
-});
-
-nhk.on(nhk.eventTypes.mouseKeyPressed, (eventData: any) => {
-    console.log('mouseKeyPressed', eventData.keyCode);
-});
-
-nhk.on(nhk.eventTypes.hotKeyTriggered, (eventData: any) => {
-    console.log('hotKeyTriggered', eventData.macroName);
-});
-
-nhk.on(nhk.eventTypes.hotstringTriggered, (eventData: any) => {
-    console.log('hotstringTriggered', eventData.macroName);
-});
-
 // nhk.stopListening(); // terminates the program
