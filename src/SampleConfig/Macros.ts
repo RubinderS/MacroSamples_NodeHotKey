e997fdd5cf2673968b557c79ebeb39ec11c62d5f
Rubinder25 <mail2rubinder@gmail.com>
2019-05-10 16:17:06 +1000

    updated macros

--- src/SampleConfig/Macros.ts
+++ src/SampleConfig/Macros.ts
@@ -38,7 +38,7 @@ export let Macros: MacroType = {
 		steps: [{ paste: `"The quick brown fox jumps over the lazy dog" is an English-language pangram—a sentence that contains all of the letters of the alphabet. It is commonly used for touch-typing practice, testing typewriters and computer keyboards, displaying examples of fonts, and other applications involving text where the use of all letters in the alphabet is desired. Owing to its brevity and coherence, it has become widely known.` }]
 	},
 
-	// Macro 5: press ALT+F to type current date 
+	// Macro 5: press ALT+F to type current date using a javascript function
 	'Custom Function': {
 		hotkeys: [kc._F, kc._ALT],
 		conditions: {
